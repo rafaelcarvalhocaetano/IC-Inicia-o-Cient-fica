998e87a6ebba58410e461886c5674eb7243a4362
Rafael <rapha.pse@outlook.com>
2017-09-15 19:51:12 -0300

    Adicionando programa no arduino para comunicação serial 9600 com java

--- arduino-read-rfid/arduino-read-rfid.ino
+++ arduino-read-rfid/arduino-read-rfid.ino
@@ -6,22 +6,36 @@
 
 
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); 
+MFRC522 r(SS_PIN, RST_PIN);
 
-
-
-//*****************************************************************************************//
-void setup() {
-  Serial.begin(9600);                                           // Initialize serial communications with the PC
-  SPI.begin();                                                  // Init SPI bus
-  mfrc522.PCD_Init();                                              // Init MFRC522 card
-  Serial.println(F("Read personal data on a MIFARE PICC:"));    //shows in serial that it is ready to read
+void setup(){
+  Serial.begin(9600);
+  SPI.begin();
+  r.PCD_Init();
 }
+void loop(){
+  leitura();
+}
+void leitura(){
+  String conteudo = "";
 
-//*****************************************************************************************//
-void loop() {
-
+  //Verifica se existe alguma tag próxima ou Lendo
+  if(!r.PICC_IsNewCardPresent() || !r.PICC_ReadCardSerial()){
+    delay(50);
+    return;
+  }
   
+  
+  //Faz a leitura e a concatenação dos valores obtidos 
+  for (byte i = 0; i < r.uid.size; i++)
+  {
+    conteudo.concat(String(r.uid.uidByte[i]<0x10 ? " 0" : " "));
+    conteudo.concat(String(r.uid.uidByte[i], HEX));
 
-
+  }
+  Serial.println(conteudo);
+  
 }
+
+
+
